46301fcbcde754b9766600dae32e3d7ebf7e5807
sssleepwalker <zagagylka87@gmail.com>
2020-06-08 16:03:01 +0500

    задание 2

---
+++ zadanie2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using static lib2;
+namespace zadanie2
+{
+    class Program
+    { 
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Задание №1");
+            string po = "";
+            char ws = 'f';
+            Console.WriteLine("Введите строку: ");
+            po = Console.ReadLine();
+            po = po.ToLower();
+            int kj = 0;
+            int fc = 0;
+            for (int i = 0; i < po.Length; i++)
+            {
+                if (po[i] == ws) kj++;
+                if (kj == 2)
+                {
+                    fc = i;
+                }
+            }
+            if (kj == 0) Console.WriteLine("-2");
+            else if (kj == 1) Console.WriteLine("-1");
+            else if (kj > 1) Console.WriteLine(fc +"\n");
+
+            Console.WriteLine("\n" + "Задание №2" + "\n" + lib2.dva("ЁжИк") + "\n");
+
+            Console.WriteLine("\n" + "Задание №3");
+            Console.WriteLine("Введите строку: ");
+            string pk = "";
+            pk = Console.ReadLine();
+            int ki = pk.IndexOf("h");
+            int ho = pk.LastIndexOf("H");
+            string kiho = "";
+            for (int min = 0; min < pk.Length; min++)
+            {
+                string won = (pk[min] + "");
+                if (String.Compare("h", won) == 0 && min != ki && min != ho) kiho = kiho + "H";
+                else kiho = kiho + pk[min];
+            }
+            Console.WriteLine(kiho);
+
+
+        }
+
+
+    }
+   
+    
+}
+
+            
+
+            
+
+        
---
+++ zadanie2/lib2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography;
+
+public class lib2
+{
+    
+    static public string dva(string v)
+    {
+        string gg = "";
+        string[] rus = new string[] {"А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р" ,"С",
+                "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я",
+        "а", "б", "в", "г", "д", "е", "ё", "ж", "з","и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", 
+            "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"};
+
+
+        string[] eng = new string[] {"A", "B", "V", "G", "D", "E", "E", "ZH", "Z", "I", "Y", "K", "L", "M", "N", "O", "P", "R", "S",
+                "T","U","F","KH","TS","CH","SH","SHCH",null,"Y",null,"E","YU","YA",
+        "a","b","v","g","d","e","e","zh","z","i","y","k","l","m","n","o","p","r","s","t","u","f","kh","ts","ch",
+            "sh","shch", null, "y", null, "e","yu","ya"};
+
+
+        for (int cc = 0; cc < v.Length; cc++)
+            for (int zz = 0; zz < rus.Length; zz++)
+                if (v.Substring(cc, 1) == rus[zz]) gg = gg + eng[zz];
+        return gg;
+    }
+   
+   
+}
